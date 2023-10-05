cd9bef0aa0ce59837f1bf2fce06e71fdf1dbd286
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-10-05 10:53:23 +0200

    Oprava tlačítek html a doplnění JS

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 let currentPlayer = 'circle';
 
-const play = (event, target) => {
-  currentPlayer === 'circle';
-  event.target.classList.add('game-buttons__circle');
-  event.target.disabled = true;
+const play = (event) => {
+  if (currentPlayer === 'circle') {
+    event.target.classList.add('game-buttons__circle');
+    document.querySelector('#circle_cross').src = 'img/cross.svg';
+    currentPlayer = 'cross';
+    event.target.disabled = true;
+  } else {
+    event.target.classList.add('game-buttons__cross');
+    document.querySelector('#circle_cross').src = 'img/circle.svg';
+    currentPlayer = 'circle';
+    event.target.disabled = true;
+  }
 };
+
 document.querySelector('button:nth-child(1)').addEventListener('click', play);
 document.querySelector('button:nth-child(2)').addEventListener('click', play);
 document.querySelector('button:nth-child(3)').addEventListener('click', play);
@@ -15,3 +24,14 @@ document.querySelector('button:nth-child(7)').addEventListener('click', play);
 document.querySelector('button:nth-child(8)').addEventListener('click', play);
 document.querySelector('button:nth-child(9)').addEventListener('click', play);
 document.querySelector('button:nth-child(10)').addEventListener('click', play);
+
+//Bonus
+
+const homeElement = document.querySelector('#home');
+const reboot = (event) => {
+  const question = confirm('Opravdu chcete odejit?');
+  if (question === false) {
+    event.preventDefault();
+  }
+};
+homeElement.addEventListener('click', reboot);
